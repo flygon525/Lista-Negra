8dd20800393c89e233a828afe4587172e97a34c1
flygon525 <vianathiago171@gmail.com>
2021-12-06 09:49:43 -0300

    ok

---
+++ lista/src/home/index.js
@@ -0,0 +1,19 @@
+
+
+export default function Lista(){
+    return(
+        <div>
+            <div>Novo</div>
+
+            <div>Nome:</div>
+            <input/>
+            <button> ok </button>
+
+            <div>Listar</div>
+
+            <div><div>id:</div> 1 </div>
+
+            <div><div>nome:</div> cara </div>
+        </div>
+    )
+}
